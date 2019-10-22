eb11ec3e37213a78f9d50cb285ce4e50e715962e
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2019-10-23 02:52:46 +0900

    create 追加

--- mytodo/urls.py
+++ mytodo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TaskListView
+from .views import TaskListView, TaskCreateView
 
 app_name = 'mytodo'
 
 urlpatterns = [
     path('',TaskListView.as_view(), name='task-list'),
+    path('create',TaskCreateView.as_view(), name='task-create'),
 ]
--- mytodo/views.py
+++ mytodo/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
+from django.urls import reverse_lazy
 
 from mytodo.models import Task
 
 class TaskListView(ListView):
     model = Task
     template_name = 'mytodo/list.html'
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = '__all__'
+    initial = {'status':0}
+    template_name = 'mytodo/create.html'
+    success_url = reverse_lazy('mytodo:task-list')
